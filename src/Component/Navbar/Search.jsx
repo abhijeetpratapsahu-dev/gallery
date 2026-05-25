7aa7b153a615ddc951a5fd9d461a43c851bd92ae
Abhijeet Sahu <abhijeetpratapsahu@gmail.com>
2026-05-25 17:57:37 +0530

    updated website

--- src/Component/Navbar/Search.jsx
+++ src/Component/Navbar/Search.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import {House} from 'lucide-react'
 
 const Search = () => {
   return (
     <div>
+        <House className='absolute left-2  bottom-141 cursor-pointer hover:text-blue-300' onClick={function(){
+          window.location.href = "/"
+      }}/>
+
     <h1 className="text-white absolute left-150 top-50">
         NO DATA FOUND
     </h1>
