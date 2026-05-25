81d6bfdfd78b017a563baf7403f54c9e9826dc48
Abhijeet Sahu <abhijeetpratapsahu@gmail.com>
2026-05-25 17:55:13 +0530

    updated website

--- src/Component/Navbar/Search.jsx
+++ src/Component/Navbar/Search.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Search = () => {
   return (
     <div>
-    <h1>
+    <h1 className="text-white absolute left-150 top-50">
         NO DATA FOUND
     </h1>
     </div>
