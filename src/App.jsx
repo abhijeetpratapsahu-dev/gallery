d2f5ded112d1dfc51ef4f1023ac1a6ea01e0bd54
Abhijeet Sahu <abhijeetpratapsahu@gmail.com>
2026-05-27 18:14:14 +0530

    updated website

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
   return (
-    <div className='w-auto h-auto bg-black text-white flex flex-col justify-start'>
+    <div className='w-full h-full bg-black text-white flex flex-col justify-start'>
       <Analytics />
 
       <Nav />
