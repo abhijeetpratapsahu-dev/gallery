08442ab499a847ddff7470857a430f1ff6ca6b96
Abhijeet Sahu <abhijeetpratapsahu@gmail.com>
2026-05-31 18:10:03 +0530

    updated website

--- src/Component/Navbar/Img/img.jsx
+++ src/Component/Navbar/Img/img.jsx
@@ -3,7 +3,7 @@ import img1 from '../../../assets/cd.jpg'
 const img = () => {
   return (
     <div>
-      <img className='h-70 w-90 absolute left-35 top-68' src={img1}/>
+      <img className='h-70 w-90 absolute left-40 top-68' src={img1}/>
     </div>
   )
 }
