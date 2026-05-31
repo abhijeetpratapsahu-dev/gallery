900fa8a2da8d5b4cb56488355655d4220113520e
Abhijeet Sahu <abhijeetpratapsahu@gmail.com>
2026-05-31 18:20:50 +0530

    updated website

--- src/Component/Navbar/Banner/Bann.jsx
+++ src/Component/Navbar/Banner/Bann.jsx
@@ -2,7 +2,7 @@ import abc from '../../../assets/abc.jpg';
 
 const Bann = () => {
   return (
-    <div className='h-[150px] w-[430px] '>
+    <div className='h-[150px] w-[430px] rounded-2xl'>
       <img className='h-full w-full relative left-[468px] top-[20px]'src={abc}/>
     </div>
   )
