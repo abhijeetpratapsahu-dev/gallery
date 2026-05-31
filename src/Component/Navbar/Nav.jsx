62a5d92b97efa4a2781aed22f65be9ec0f90c1b3
Abhijeet Sahu <abhijeetpratapsahu@gmail.com>
2026-05-31 16:29:57 +0530

    updated website

--- src/Component/Navbar/Nav.jsx
+++ src/Component/Navbar/Nav.jsx
@@ -15,18 +15,18 @@ const Nav = () => {
     <div>
       <nav className='p-2 border-b border-gray-300'>
         <div className='flex gap-8 cursor-pointer'>
-          <Link to="/about" className='font-roboto hover:text-green-300'>ABOUT ER.AVINASH SAHU</Link>
-          <Link to="/gallery" className='font-roboto hover:text-green-300'>GALLERY</Link>
-          <Link to="/tours" className='font-roboto hover:text-green-300'>TOURS</Link>
-          <Link to="/press" className='font-roboto hover:text-green-300'>PRESS</Link>
-          <Link to="/contact" className='font-roboto hover:text-green-300'>CONTACT US</Link>
-          <Link to="/videos" className='font-roboto hover:text-green-300'>VIDEOS</Link>
-          <Link to="/helping-hand" className='font-roboto hover:text-green-300'>HELPING HAND</Link>
-          <Link to="/profile" className='font-roboto hover:text-green-300'>ER.AVINASH SAHU PROFILE</Link>
+          <Link to="/about" className='font-roboto hover:text-green-600'>ABOUT ER.AVINASH SAHU</Link>
+          <Link to="/gallery" className='font-roboto hover:text-green-600'>GALLERY</Link>
+          <Link to="/tours" className='font-roboto hover:text-green-600'>TOURS</Link>
+          <Link to="/press" className='font-roboto hover:text-green-600'>PRESS</Link>
+          <Link to="/contact" className='font-roboto hover:text-green-600'>CONTACT US</Link>
+          <Link to="/videos" className='font-roboto hover:text-green-600'>VIDEOS</Link>
+          <Link to="/helping-hand" className='font-roboto hover:text-green-600'>HELPING HAND</Link>
+          <Link to="/profile" className='font-roboto hover:text-green-600'>ER.AVINASH SAHU PROFILE</Link>
         </div>
 
         <div className="absolute left-270 top-2">
-          <input className="w-50 border-white" type="search" placeholder="Search" onChange={(e) => setSearch(e.target.value)} onKeyDown={handleKeyDown}/>
+          <input className="w-50 border-black" type="search" placeholder="Search" onChange={(e) => setSearch(e.target.value)} onKeyDown={handleKeyDown}/>
           <div className='cursor-pointer absolute left-[220px] top-[2px] hover:text-red-300'>
             <Search />
           </div>
