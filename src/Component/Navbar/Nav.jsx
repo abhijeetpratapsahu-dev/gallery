c53eca87648fdfe4983d3629e7a5217dcc13562a
Abhijeet Sahu <abhijeetpratapsahu@gmail.com>
2026-06-01 15:43:16 +0530

    updated website

--- src/Component/Navbar/Nav.jsx
+++ src/Component/Navbar/Nav.jsx
@@ -27,7 +27,7 @@ const Nav = () => {
 
         <div className="absolute left-276 top-2">
           <input className="w-50 border-black" type="search" placeholder="Search" onChange={(e) => setSearch(e.target.value)} onKeyDown={handleKeyDown}/>
-          <div className='cursor-pointer absolute left-[213px] top-[2px] hover:text-red-500'>
+          <div className='cursor-pointer absolute left-[210px] top-[2px] hover:text-red-500'>
             <Search />
           </div>
         </div>
