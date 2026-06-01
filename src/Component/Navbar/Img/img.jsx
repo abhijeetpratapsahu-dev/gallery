d8a7a1a1ce20576c6b4229c0de2fef8a88473782
Abhijeet Sahu <abhijeetpratapsahu@gmail.com>
2026-06-01 12:50:11 +0530

    updated website

--- src/Component/Navbar/Img/img.jsx
+++ src/Component/Navbar/Img/img.jsx
@@ -2,7 +2,7 @@ import img1 from '../../../assets/cd.jpg'
 
 const img = () => {
   return (
-    <div className='h-40 w-70 rounded-4xl'>
+    <div className='h-40 w-70 bg-black rounded-4xl'>
       <img className='h-full w-full rounded-4xl absolute left-40 top-68' src={img1}/>
     </div>
   )
