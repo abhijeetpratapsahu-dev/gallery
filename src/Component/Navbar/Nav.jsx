9f80d5b6ed4c5ae5bb76b2cde813b90852f2edd5
Abhijeet sahu <abhijeetsahupratap@gmail.com>
2026-06-25 04:03:31 -0700

    updated website

--- src/Component/Navbar/Nav.jsx
+++ src/Component/Navbar/Nav.jsx
@@ -27,7 +27,7 @@ const Nav = () => {
 
         <div className="absolute left-276 top-1.5">
           <input className="w-50 px-1 border-b border-black" type="search" placeholder="Search" onChange={(e) => setSearch(e.target.value)} onKeyDown={handleKeyDown}/>
-          <div className='cursor-pointer absolute left-[210px] top-[2px] hover:text-red-500'>
+          <div className='cursor-pointer absolute left-[210px] top-[2px] hover:text-red-800'>
             <Search />
           </div>
         </div>
--- src/Home.jsx
+++ src/Home.jsx
@@ -4,13 +4,13 @@ import Iq from './Component/Navbar/Img/img'
 
 const Home = () => {
   return (
-    <>
+    <div className='bg-white min-h-full min-w-full text-black relative'>
       <Bann />
-      <div className='w-full max-w-7xl mx-auto p-4 flex flex-col md:flex-row gap-6 items-start'>
+      <div className='w-full h-full max-w-7xl mx-auto p-4 flex flex-col md:flex-row gap-6 items-start'>
         <Left />
         <Iq />
       </div>
-    </>
+    </div>
   )
 }
 
