88ede1819eddc1cd758b08980924723cacde32f5
Abhijeet sahu <abhijeetsahupratap@gmail.com>
2026-06-25 03:35:28 -0700

    updated wbesite

--- src/Component/Navbar/Pages/profile.jsx
+++ src/Component/Navbar/Pages/profile.jsx
@@ -31,7 +31,7 @@ const Home = () => {
           जनता के प्रति उनका समर्पण और सेवा भावना उन्हें अन्य नेताओं से अलग पहचान दिलाती है।
         </h1>
       </div>
-      <div className='absolute left-4 bottom-[35vh] border-2 border-black p-3'>
+      <div className='absolute left-4 bottom-[55vh] border-2 border-black p-3'>
           <a href='https://www.facebook.com/dravinashsahu/' target="_blank"  rel="noopener noreferrer" className='hover:text-blue-900'>
           <FaFacebookF size={30} />
         </a>
