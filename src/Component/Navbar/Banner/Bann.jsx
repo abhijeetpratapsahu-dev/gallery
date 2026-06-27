fea82a76fb8058f561b1c24d204a19fcb212362e
Abhijeet sahu <abhijeetsahupratap@gmail.com>
2026-06-27 06:17:23 -0700

    updated website

--- src/Component/Navbar/Banner/Bann.jsx
+++ src/Component/Navbar/Banner/Bann.jsx
@@ -2,8 +2,13 @@ import abc from '../../../assets/abc.jpg';
 
 const Bann = () => {
   return (
-    <div className='h-[150px] w-[430px] rounded-2xl'>
-      <img className='h-full w-full rounded-2xl relative left-[468px] top-[20px]'src={abc}/>
+     <div className="flex justify-center pt-5">
+      <div className="h-[150px] w-[430px]">
+        <img
+          src={abc}
+          className="h-full w-full rounded-2xl"
+        />
+      </div>
     </div>
   )
 }
